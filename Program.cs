a2e4826cabf874dc029994fdeb94ed326a40b276
Abdi Farah <abdirahman.farah@live.se>
2023-01-19 16:07:25 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace grupp_tiger2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
