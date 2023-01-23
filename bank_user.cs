52e27e64a0aa2a55e5c75f74b185fdb64c40720c
FreeHugZz <adrian.notaj@chasacademy.se>
2023-01-23 12:43:04 +0100

    Installed DBgate and Postgres applications with Dapper, Npgsql and System.configration packages. The ConnectionString and the actual connection between the database should work now.

---
+++ bank_user.cs
@@ -0,0 +1,10 @@
+﻿namespace grupp_tiger2
+{
+    internal class bank_user
+    {
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public string pin_code { get; set; }
+
+    }
+}
