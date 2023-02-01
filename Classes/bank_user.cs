1f585972118fc231b17d8f398b452bc48adabe4e
Abdi Farah <abdirahman.farah@live.se>
2023-02-01 16:40:31 +0100

    bank user property changed

--- Classes/bank_user.cs
+++ Classes/bank_user.cs
@@ -7,6 +7,21 @@
         public string last_name { get; set; }
         public string pin_code { get; set; }
         public string username { get; set; }
+        public int roleID { get; set; }
+        public int branchID { get; set; }
 
+        public bank_user(string first_name, string last_name, string pin_code, string username, int roleID, int branchID)
+        {
+            this.first_name = first_name;
+            this.last_name = last_name;
+            this.pin_code = pin_code;
+            this.username = username;
+            this.roleID = roleID;
+            this.branchID = branchID;
+        }
+
+        public bank_user()
+        {
+        }
     }
 }
