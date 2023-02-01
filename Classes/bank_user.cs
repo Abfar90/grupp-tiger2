b7548160b216e4a9692184272d8c4fc2d9a522ef
Abdi Farah <abdirahman.farah@live.se>
2023-02-01 15:02:38 +0100

    Added properties to user and account class. Almost complete create user function in dataaccess class

--- Classes/bank_user.cs
+++ Classes/bank_user.cs
@@ -7,6 +7,24 @@
         public string last_name { get; set; }
         public string pin_code { get; set; }
         public string username { get; set; }
+        public string roleID { get; set; }
+        public string branchID { get; set; }
+        public string userName { get; set; }
 
+        public bank_user(int id, string first_name, string last_name, string pin_code, string username, string roleID, string branchID, string userName)
+        {
+            this.id = id;
+            this.first_name = first_name;
+            this.last_name = last_name;
+            this.pin_code = pin_code;
+            this.username = username;
+            this.roleID = roleID;
+            this.branchID = branchID;
+            this.userName = userName;
+        }
+
+        public bank_user()
+        {
+        }
     }
 }
--- Data/PostgresDataAccess.cs
+++ Data/PostgresDataAccess.cs
@@ -6,6 +6,9 @@ namespace grupp_tiger2.Data
 {
     internal class PostgresDataAccess
     {
+        private static List<NpgsqlParameter> parameters;
+        static string connectionString = ConfigurationManager.ConnectionStrings["postgres"].ConnectionString;
+
         public static List<bank_user> LoadBankUsers()
         {
             // This is the connection string from the app.config file, and "postgres" is the name of the connection string
@@ -56,6 +59,28 @@ namespace grupp_tiger2.Data
                 // This is the Dapper method that executes the query and returns a list of bank_user objects
                 var output = conn.Query<bank_user>("select * from bank_user", new DynamicParameters());
                 return output.ToList();
+
+            }
+        }
+
+        public static void CreateUser(bank_user user)
+        {
+            // This is the connection string from the app.config file, and "postgres" is the name of the connection string
+            string connectionString = ConfigurationManager.ConnectionStrings["postgres"].ConnectionString;
+            parameters.Add(new NpgsqlParameter("@first_name", user.first_name));
+            parameters.Add(new NpgsqlParameter("@last_name", user.last_name));
+            parameters.Add(new NpgsqlParameter("@pin_code", user.pin_code));
+            parameters.Add(new NpgsqlParameter("@role_id", user.roleID));
+            parameters.Add(new NpgsqlParameter("@branch_id", user.branchID));
+            parameters.Add(new NpgsqlParameter("@username", user.username));
+            // using is a C# feature that automatically closes the connection when the code inside the block is done
+            using (NpgsqlConnection conn = new NpgsqlConnection(connectionString))
+            {
+                // Opens the connection to the database
+                conn.Open();
+
+                // This is the Dapper method that executes the query and returns a list of bank_user objects
+                conn.Query<bank_user>("INSERT INTO \"public\".\"bank_user\" (\"first_name\", \"last_name\", \"pin_code\", \"role_id\", \"branch_id\", \"username\") VALUES ("user.first_name,user.last_name,user.pin_code,user.roleID,user.branchID,user.userName");", new DynamicParameters());
 
             }
         }
